7c7cd7e112ff9f4c4bb9f224f785508c31b8bbdc
kruvv <kruvv@mail.ru>
2021-05-21 17:37:15 +0300

    Добавил  возможность сохранения  задач в localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar } from "./components/Navbar";
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import { ITodo } from "./interfaces";
 
+declare var confirm: (question: string) => boolean;
+
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem("todos") || "[]") as ITodo[];
+    setTodos(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addHandler = (title: string) => {
     const newTodo: ITodo = {
@@ -18,20 +29,39 @@ const App: React.FC = () => {
   };
 
   const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
 
-  }
+  const removeHandler = (event: React.MouseEvent, id: number) => {
+    event.preventDefault();
 
-  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Вы уверены, что хотите удалить эту задачу?");
 
-  }
-
+    if (shouldRemove) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
 
   return (
     <>
       <Navbar />
       <div className="container">
         <TodoForm onAdd={addHandler} />
-        <TodoList todos={todos} />
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
       </div>
     </>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,43 @@
-import { ITypeOpts } from "@testing-library/user-event";
 import React from "react";
 import { ITodo } from "../interfaces";
 
 type TodoListProps = {
   todos: ITodo[];
+  onToggle(id: number): void;
+  onRemove: (event: React.MouseEvent, id: number) => void;
 };
 
-export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onToggle,
+  onRemove,
+}) => {
+    if (todos.length === 0) {
+        return <div className="center">Ваш лист задач пуст :(</div>
+    }
   return (
     <ul>
       {todos.map((todo) => {
         const classes = ["todo"];
         if (todo.completed) {
-            classes.push('completed')
+          classes.push("completed");
         }
 
         return (
-          <li className={classes.join(' ')} key={todo.id}>
-            <label htmlFor="">
-              <input type="checkbox" checked={todo.completed} />
+          <li className={classes.join(" ")} key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={onToggle.bind(null, todo.id)}
+              />
               <span>{todo.title}</span>
-              <i className="material-icons red-text">delete</i>
+              <i
+                className="material-icons red-text"
+                onClick={(event) => onRemove(event, todo.id)}
+              >
+                delete
+              </i>
             </label>
           </li>
         );
